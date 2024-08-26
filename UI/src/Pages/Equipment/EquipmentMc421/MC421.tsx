e72ea90933c465bc0b08df11f17711f17c4e7c19
zero <artemlysakov793@gmail.com>
2024-08-26 19:00:13 +0300

    + mc421

--- UI/src/Pages/Equipment/EquipmentMc421/MC421.tsx
+++ UI/src/Pages/Equipment/EquipmentMc421/MC421.tsx
@@ -1,5 +1,6 @@
 import Navigation from "../../../Components/Navigation/Navigation";
 import { useState, useEffect } from 'react';
+import s from './MC421.module.css'
 
 interface LogEntry {
     timestamp: string;
@@ -12,15 +13,26 @@ interface LogEntry {
 const EquipMC421 = () => {
 
     const [warningCount, setWarningCount] = useState<number>(0);
+    const [filteredCount, setFilteredCount] = useState<number>(0);
 
     useEffect(() => {
       fetch('/error.json')
         .then(response => response.json())
         .then((data: LogEntry[]) => {
+
           const warnings = data.filter(item => item.level === 'WARNING');
           setWarningCount(warnings.length);
+
+          // Фильтруем данные, чтобы найти записи, где message содержит "Head4" и "F35"
+          const filteredEntries = data.filter(item =>
+          item.message.includes("Head4") && item.message.includes("F35") && item.level === 'WARNING');
+          setFilteredCount(filteredEntries.length);
+
+          
         })
         .catch(error => console.error('Ошибка при загрузке данных:', error));
+
+        
     }, []); 
 
     return (
@@ -29,11 +41,14 @@ const EquipMC421 = () => {
 
             <div>
                 <h2>Статистика MC421</h2>
+
+                <ul>
+                  <li>Количество записей с уровнем WARNING: {warningCount}</li>  
+                  <li>Количество записей, где "Head4" "F35" и статус WARNING: {filteredCount}</li>  
+                </ul>
             </div>
 
-            <div>
-                Количество записей с уровнем WARNING: {warningCount}
-            </div>
+
         </div>
     );
 };
