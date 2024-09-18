7681caa73de56493177385bd9022b3f2dc74c78b
zero <artemlysakov793@gmail.com>
2024-09-18 08:53:33 +0300

    mini save

--- MaterialUI/src/Components/CM421/CM421Components/ErrorCodesComponent.tsx
+++ MaterialUI/src/Components/CM421/CM421Components/ErrorCodesComponent.tsx
@@ -180,3 +180,5 @@ const ErrorCodesComponent = () => {
 };
 
 export default ErrorCodesComponent;
+
+
--- MaterialUI/src/Components/Graphs/Column/ErrorCodesColumn.tsx
+++ MaterialUI/src/Components/Graphs/Column/ErrorCodesColumn.tsx
@@ -1,51 +1,56 @@
-import React from 'react';
-import ReactApexChart from "react-apexcharts";
+  import React from 'react';
+  import ReactApexChart from "react-apexcharts";
 
-interface ErrorCodesColumnProps {
-  errorCodes: Record<string, { count: number; description: string }>;
-}
+  interface ErrorCodesColumnProps {
+    errorCodes: Record<string, { count: number; description: string }>;
+  }
 
-const ErrorCodesColumn: React.FC<ErrorCodesColumnProps> = ({ errorCodes }) => {
-  const chartData = {
-    options: {
-      xaxis: {
-        categories: Object.keys(errorCodes),
-        labels: {
-          rotate: -90,
-          rotateAlways: true,
+  const ErrorCodesColumn: React.FC<ErrorCodesColumnProps> = ({ errorCodes }) => {
+    const chartData = {
+      options: {
+        xaxis: {
+          categories: Object.keys(errorCodes),
+          labels: {
+            rotate: -90,
+            rotateAlways: true,
+            style: {
+              fontSize: '9px',
+              fontFamily: 'Helvetica, Arial, sans-serif',
+              fontWeight: 400,
+          },
+          },
         },
-      },
-      yaxis: {
-        title: {
-          text: 'Количество',
+        yaxis: {
+          title: {
+            text: 'Количество',
+          },
         },
-      },
-      plotOptions: {
-        bar: {
-          horizontal: false,
+        plotOptions: {
+          bar: {
+            horizontal: false,
+          },
         },
+        dataLabels: {
+          enabled: false,
+        },
+        colors: ['#008FFB'],
       },
-      dataLabels: {
-        enabled: false,
-      },
-      colors: ['#008FFB'],
-    },
-    series: [{
-      name: 'Количество',
-      data: Object.values(errorCodes).map(({ count }) => count),
-    }],
+      series: [{
+        name: 'Количество',
+        data: Object.values(errorCodes).map(({ count }) => count),
+      }],
+    };
+
+    return (
+      <div style={{ marginTop: '20px' }}>
+        <ReactApexChart 
+          options={chartData.options} 
+          series={chartData.series} 
+          type="bar" 
+          height={400}
+        />
+      </div>
+    );
   };
 
-  return (
-    <div style={{ marginTop: '20px' }}>
-      <ReactApexChart 
-        options={chartData.options} 
-        series={chartData.series} 
-        type="bar" 
-        height={400}
-      />
-    </div>
-  );
-};
-
-export default ErrorCodesColumn;
+  export default ErrorCodesColumn;
